69c057e33b2f14421a4bf331e21c66d66588df99
aparichit0 <0.dharmveersingh@gmail.com>
2022-04-18 23:05:03 +0530

    passed all tests
    added comments,
    format code
    cleared all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,22 @@
-const leapYears = function() {
-
+const leapYears = function () {
+    const year = arguments[0];
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                //divisable by 400
+                return true;
+            } else {
+                //divisable by 100 but not by 400
+                return false;
+            };
+        } else {
+            //divisable by 4 only
+            return true;
+        };
+    } else {
+        //not divisable by 4
+        return false;
+    };
 };
 
 // Do not edit below this line
