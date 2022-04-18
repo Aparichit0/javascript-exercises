c1c94efee82cba49968a89538f2356105591cd6d
aparichit0 <0.dharmveersingh@gmail.com>
2022-04-18 22:27:15 +0530

    all test passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
-
+const sumAll = function () {
+    if (Number.isInteger(arguments[0]) && Number.isInteger(arguments[1])) {
+        if (arguments[0] >= 0 && arguments[1] >= 0) {
+            let smallNum;
+            let bigNum;
+            //determine which argument is small and which one is big
+            if (arguments[0] < arguments[1]) {
+                smallNum = arguments[0];
+                bigNum = arguments[1]
+            } else {
+                smallNum = arguments[1];
+                bigNum = arguments[0]
+            };
+            let sum = 0;
+            for (let i = smallNum; i <= bigNum; i++) {
+                sum += i;
+            }
+            return sum;
+        } else {
+            //if any or both are negative integers
+            return "ERROR";
+        };
+    } else {
+        //if any or both are not integers
+        return "ERROR";
+    };
 };
 
 // Do not edit below this line
