45473d0dd52611222a1fdeb83b5a5a1043b7d7eb
aparichit0 <0.dharmveersingh@gmail.com>
2022-04-18 22:34:09 +0530

    code refactor
    removed useless codeblock
    added better comments

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,20 +1,19 @@
 const sumAll = function () {
     if (Number.isInteger(arguments[0]) && Number.isInteger(arguments[1])) {
+        //both postive integers
         if (arguments[0] >= 0 && arguments[1] >= 0) {
-            let smallNum;
-            let bigNum;
-            //determine which argument is small and which one is big
-            if (arguments[0] < arguments[1]) {
-                smallNum = arguments[0];
-                bigNum = arguments[1]
-            } else {
+            //assuming first argument is small
+            let smallNum = arguments[0];
+            let bigNum = arguments[1];
+            //if first argument is big
+            if (arguments[0] > arguments[1]) {
                 smallNum = arguments[1];
                 bigNum = arguments[0]
             };
             let sum = 0;
             for (let i = smallNum; i <= bigNum; i++) {
                 sum += i;
-            }
+            };
             return sum;
         } else {
             //if any or both are negative integers
