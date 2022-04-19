39d0ff2ff02048ba1bb8f4afaa6274d42801207d
aparichit0 <0.dharmveersingh@gmail.com>
2022-04-19 19:12:07 +0530

    ctof passed all tests
    ctof = celsius to fahrenheit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -7,6 +7,11 @@ const ftoc = function() {
 };
 
 const ctof = function() {
+  const celsiusTemp = arguments[0];
+  let fahrenheitTemp = celsiusTemp * 9/5 + 32;
+  //roundup to one decimmal value
+  let roundUp = Math.floor(fahrenheitTemp * (-10)) / (-10);
+  return roundUp;
 
 };
 
