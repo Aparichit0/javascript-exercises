c065a6a5c779ab31cac55d1dc97a32de4aecc6f2
aparichit0 <0.dharmveersingh@gmail.com>
2022-04-19 22:46:23 +0530

    punctuations and multiword support

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,10 +1,17 @@
 const palindromes = function () {
     const textArray = arguments[0].split("");
-    const lastPosition = textArray.length - 1;
+    //ignore punctuation
+    const filterArray = [];
+    const ignoreArray = ['!', '.', ','];
+    textArray.forEach(char => {
+    if (!ignoreArray.includes(char)) filterArray.push(char);     
+    });
+
+    const lastPosition = filterArray.length - 1;
     const midPoint = Math.floor(lastPosition / 2);
     //compare first half with last half in reverse
     for (let i = 0; i < midPoint; i++) {
-        if (textArray[i] === textArray[lastPosition - i]) return true;
+        if (filterArray[i] === filterArray[lastPosition - i]) return true;
         else return false;
     };
 };
