ae562f4ff8bdc904e7887f3a39f8f75235538a7c
aparichit0 <0.dharmveersingh@gmail.com>
2022-04-19 23:15:58 +0530

    Bug fix!
    not all cases will return true anymore if just the middle portion is same

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -2,18 +2,23 @@ const palindromes = function () {
     const textArray = arguments[0].toLowerCase().split("");
     //ignore punctuation
     const filterArray = [];
-    const ignoreArray = ['!', '.', ','];
+    const ignoreArray = ['!', '.', ',', ' '];
     textArray.forEach(char => {
-    if (!ignoreArray.includes(char)) filterArray.push(char);     
+        if (!ignoreArray.includes(char)) filterArray.push(char);
     });
 
     const lastPosition = filterArray.length - 1;
     const midPoint = Math.floor(lastPosition / 2);
+    let isPalindrome = true;
     //compare first half with last half in reverse
-    for (let i = 0; i < midPoint; i++) {
-        if (filterArray[i] === filterArray[lastPosition - i]) return true;
-        else return false;
+    palindromeCheck: for (let i = 0; i < midPoint; i++) {
+        if (filterArray[i] !== filterArray[lastPosition - i]) {
+            isPalindrome = false;
+            //break at first enconter of false
+            break palindromeCheck;
+        };
     };
+    return isPalindrome;
 };
 
 // Do not edit below this line
