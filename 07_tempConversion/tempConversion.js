9c306ab9fcc1d7e1b073c5b8689df0f903cd9a5f
aparichit0 <0.dharmveersingh@gmail.com>
2022-04-19 19:03:36 +0530

    ftoc passed all tests
    ftoc = fahrenheit to celsius

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,5 +1,9 @@
 const ftoc = function() {
-
+  const fahrenheitTemp = arguments[0];
+  let celsiusTemp = (fahrenheitTemp - 32) * 5/9;
+  //roundup to one decimmal value
+  let roundUp = Math.floor(celsiusTemp * (-10)) / (-10);
+  return roundUp;
 };
 
 const ctof = function() {
