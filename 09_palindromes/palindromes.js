c05496560099ff81ba18cbd43d9ac327d1948aef
aparichit0 <0.dharmveersingh@gmail.com>
2022-04-19 22:19:25 +0530

    single word polindromes supported

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
 const palindromes = function () {
-
+    const textArray = arguments[0].split("");
+    const lastPosition = textArray.length - 1;
+    const midPoint = Math.floor(lastPosition / 2);
+    //compare first half with last half in reverse
+    for (let i = 0; i < midPoint; i++) {
+        if (textArray[i] === textArray[lastPosition - i]) return true;
+        else return false;
+    };
 };
 
 // Do not edit below this line
