950b1a06ef1e9b49a41ae6758bc36bffb52427fb
aparichit0 <0.dharmveersingh@gmail.com>
2022-04-19 19:16:35 +0530

    all tests passed
    format code

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,18 +1,17 @@
-const ftoc = function() {
+const ftoc = function () {
   const fahrenheitTemp = arguments[0];
-  let celsiusTemp = (fahrenheitTemp - 32) * 5/9;
+  let celsiusTemp = (fahrenheitTemp - 32) * 5 / 9;
   //roundup to one decimmal value
   let roundUp = Math.floor(celsiusTemp * (-10)) / (-10);
   return roundUp;
 };
 
-const ctof = function() {
+const ctof = function () {
   const celsiusTemp = arguments[0];
-  let fahrenheitTemp = celsiusTemp * 9/5 + 32;
+  let fahrenheitTemp = celsiusTemp * 9 / 5 + 32;
   //roundup to one decimmal value
   let roundUp = Math.floor(fahrenheitTemp * (-10)) / (-10);
   return roundUp;
-
 };
 
 // Do not edit below this line
