52977a5a83c5fe25aa881c94abd3539fb5d092d9
aparichit0 <0.dharmveersingh@gmail.com>
2022-04-20 18:10:38 +0530

    Accept postive numbers only
    safely reject unexpected input such as negative numbers and/or NaN

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,10 +1,13 @@
 const fibonacci = function () {
     const fibonacciArray = [1, 1];
     const length = arguments[0];
-    for (i = 2; i < length; i++) {
-        fibonacciArray[i] = fibonacciArray[i - 2] + fibonacciArray[i - 1];
-    };
-    return fibonacciArray[length - 1];
+    if (length > 0) {
+        //only run if argument is a postive number
+        for (i = 2; i < length; i++) {
+            fibonacciArray[i] = fibonacciArray[i - 2] + fibonacciArray[i - 1];
+        };
+        return fibonacciArray[length - 1];
+    } else return "OOPS";
 };
 
 // Do not edit below this line
