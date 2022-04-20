1db37aa6c44d8ed29fbc8a8c35002c2f9154b77a
aparichit0 <0.dharmveersingh@gmail.com>
2022-04-20 17:59:13 +0530

    almost ready
    all test for positive numbers passed
    string to number auto conversion is working flawlessly

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
 const fibonacci = function() {
-
+const fibonacciArray = [1,1];
+const length = arguments[0];
+for (i = 2; i < length; i++){
+    let newNum = fibonacciArray[i - 2] + fibonacciArray[i - 1];
+    fibonacciArray.push(newNum);
+};
+return fibonacciArray[length - 1];
 };
 
 // Do not edit below this line
