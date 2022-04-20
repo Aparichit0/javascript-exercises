65db36221d10b1443cf39909cd4360c09b945fa5
aparichit0 <0.dharmveersingh@gmail.com>
2022-04-20 20:47:13 +0530

    all tests passed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
-
-};
-
+const getTheTitles = function () {
+    const booksArray = arguments[0];
+    const bookTitles = [];
+    for (let i = 0; i < booksArray.length; i++) {
+        bookTitles[i] = booksArray[i].title;
+    };
+    return bookTitles;
+}
 // Do not edit below this line
 module.exports = getTheTitles;
