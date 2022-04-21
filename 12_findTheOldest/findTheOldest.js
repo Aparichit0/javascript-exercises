ef3475b46d3743b51cc8fb1c41aee15a13950911
aparichit0 <0.dharmveersingh@gmail.com>
2022-04-22 00:03:20 +0530

    Bug fix!
    previosly it couldn't actually determine age properly
    due to age[i+1] didn't exist for last value
    which is causing to always run default code block

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,16 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function () {
     //throwing error right at the beginning
-const person = arguments[0];
-const age = [];
-let olderPerson;
-
-for (i= 0; i < person.length; i++){
-    age[i] = person[i].yearOfDeath - person[i].yearOfBirth;
-    //compare nearby person's age
-    if (age[i] > age[i+1]) return older = person[i];
-    else return olderPerson = person[i+1];
-};
-return olderPerson;
+    const person = arguments[0];
+    const age = [];
+    let olderPerson;
+    for (i = 0; i < age.length; i++) {
+        //compare nearby person's age
+        if (age[i + 1]) {
+            //if it's the first value within age array
+            if (age[i] > age[i + 1]) return older = person[i];
+            else return olderPerson = person[i + 1];
+        } else {
+            //if it's the last value within age array
+            if (age[i] > age[i - 1]) return older = person[i];
+            else return olderPerson = person[i - 1];
+        };
+    };
+    return olderPerson;
 };
 
 // Do not edit below this line
