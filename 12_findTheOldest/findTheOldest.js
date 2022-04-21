cc59746bdcc8338516231368f08edc687e4bd7f7
aparichit0 <0.dharmveersingh@gmail.com>
2022-04-21 22:21:12 +0530

    oldest person with death year defined is supported!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
 const findTheOldest = function() {
+    //throwing error right at the beginning
+const person = arguments[0];
+const age = [];
+let olderPerson;
 
+for (i= 0; i < person.length; i++){
+    age[i] = person[i].yearOfDeath - person[i].yearOfBirth;
+    //compare nearby person's age
+    if (age[i] > age[i+1]) return older = person[i];
+    else return olderPerson = person[i+1];
+};
+return olderPerson;
 };
 
 // Do not edit below this line
