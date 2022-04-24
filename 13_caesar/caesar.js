825a836e2bac8c14fb75da0bb46e08fe5aebdbd4
aparichit0 <0.dharmveersingh@gmail.com>
2022-04-24 18:38:43 +0530

    caesar prototype is ready

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,8 @@
-const caesar = function() {
-
+const caesar = function (str, shiftFactor) {
+    let shiftStr = "";
+    let CharCode = str.charCodeAt(0);
+    shiftStr += String.fromCharCode(CharCode + shiftFactor);
+    return shiftStr;
 };
 
 // Do not edit below this line
