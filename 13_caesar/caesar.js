27c15018c713ae91aaf861df2778a76d25a157b7
aparichit0 <0.dharmveersingh@gmail.com>
2022-04-24 20:39:48 +0530

    works with phrases

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,9 +1,18 @@
 const caesar = function (str, shiftFactor) {
     let shiftStr = "";
     for (let i = 0; i < str.length; i++) {
-        let CharCode = str.charCodeAt(i);
-        shiftStr += String.fromCharCode(CharCode + shiftFactor);
-    }
+        let charCode = str.charCodeAt(i);
+        let AToZ = 64 < charCode && charCode <= 90;
+        let aToz = 96 < charCode && charCode <= 122;
+        let charIndex = charCode + shiftFactor;
+
+        if (AToZ || aToz) {
+            if ((90 < charIndex && charIndex <= 96) || 122 < charIndex) charIndex -= 26;
+            shiftStr += String.fromCharCode(charIndex);
+        } else {
+            shiftStr += str.charAt(i);
+        };
+    };
     return shiftStr;
 };
 
